0b947e21a47f5beace7df06fd60fe348822c1e88
Simon Budden <simon.budden@gmail.com>
2024-07-03 14:35:20 +0100

    Boilerplay logo and text added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
+
+import wyfeLogoWhite from '../public/wyfe_logo_text_white.svg'
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
       <div className="stitched-box p-4 mb-4 text-center">
-        <h1 className="text-9xl font-bold">Wyfe</h1>
-        <h2 className="text-2xl">Winter 2024</h2>
-        <p className="text-xl">ciao@wyfe.co.uk</p>
+        <Image src={wyfeLogoWhite} alt="Wyfe Logo" width={400} className="pb-4"/>
+        <h2 className="text-2xl font-georgia-italic">Winter 2024</h2>
+        <p className="text-xl font-georgia-italic">ciao@wyfe.co.uk</p>
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        stitched: 'inset 0 0 0 2px dashed #7d7d7d', // Custom stitched effect box shadow
+      },
+      fontFamily: {
+        'big-caslon': ['"Big Caslon"', 'serif'],
+        'georgia-italic': ['Georgia', 'serif', 'italic'],
+      },
     },
   },
   plugins: [],
