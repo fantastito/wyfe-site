d32d5f9350206973b231c5ec1f5bf5aa0af3fb2f
Simon Budden <simon.budden@gmail.com>
2024-07-29 12:39:13 +0100

    Bespoke page developed

--- app/bespoke/page.js
+++ app/bespoke/page.js
@@ -1,13 +1,66 @@
 import React from "react";
+import Image from 'next/image';
 
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
+
+import TopLeftImage from '../../public/bespoke/3a66b72d47890e697456a00f571105f8.jpg'
+import TopRightImage from '../../public/bespoke/78c3906dbc503ebdcd50e2504235e0fe.jpg'
+import BottomLeftImage from '../../public/bespoke/10-harley-viera-newton-wedding-dress-fitting.jpg'
+import BottomRightImage from '../../public/bespoke/IMG_5975.jpg'
 
 const Bespoke = () => {
     return (
         <div className="flex flex-col min-h-screen relative">
             <Navbar />
-            
+
+            {/* Page content */}
+            <main className="flex-1">
+                <div className="container mx-auto px-4 max-w-screen-lg">
+                    {/* Top images */}
+                    <div className="flex justify-center gap-4 p-4">
+                        <Image 
+                            src={TopLeftImage}
+                            alt="Bespoke 1"
+                            className="w-1/2 h-auto object-cover"
+                        />
+                        <Image 
+                            src={TopRightImage}
+                            alt="Bespoke 1"
+                            className="w-1/2 h-auto object-cover"
+                        />
+                    </div>
+                    
+                    {/* Text */}
+                    <div className="px-4 py-8 text-center font-georgia-italic">
+                        <p className="text-sm md:text-base lg:text-base">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean auctor, odio id iaculis aliquet, eros nibh suscipit ex, a fringilla felis urna in dolor. In tempor risus lorem, eu ultrices dui sollicitudin eget. In id libero condimentum ante malesuada viverra. In in dui et risus commodo porttitor. Donec id ipsum enim. Donec pulvinar ut orci lacinia scelerisque. Mauris sed lorem eu elit lacinia convallis non at ante.
+                        </p>
+                    </div>
+
+                    {/* Middle images */}
+                    <div className="flex justify-center gap-4 p-4">
+                        <Image 
+                            src={BottomLeftImage}
+                            alt="Bespoke 1"
+                            className="w-1/2 h-auto object-cover"
+                        />
+                        <Image 
+                            src={BottomRightImage}
+                            alt="Bespoke 1"
+                            className="w-1/2 h-auto object-cover"
+                        />
+                    </div>
+                    
+                    {/* Text */}
+                    <div className="px-4 py-8 text-center font-georgia-italic">
+                        <p className="text-sm md:text-base lg:text-base">
+                        In venenatis, quam a sodales cursus, dui dui pulvinar nunc, ut pretium velit mi id odio. Mauris tincidunt orci ac ante mollis faucibus. Pellentesque ac augue sagittis, elementum magna nec, cursus massa. Pellentesque justo lectus, lobortis sed vulputate vel, ullamcorper nec ante. Maecenas sit amet orci at tortor gravida vehicula vel at diam. Pellentesque semper accumsan nibh, et sollicitudin lacus tincidunt a. Nunc luctus sagittis libero non porttitor.
+                        </p>
+                    </div>
+                </div>
+            </main>
+
             <Footer />
         </div>
 
--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -24,9 +24,9 @@ const Navbar = () => {
       <nav className="bg-black font-georgia-italic py-2 md:py-2 lg:py-2 text-white w-full">
         <div className="max-w-7xl mx-auto flex justify-center items-center px-4 md:px-8 lg:px-12">
           <div className="flex space-x-4 md:space-x-6 lg:space-x-8">
-            <p className="text-base md:text-lg lg:text-xl">Dresses</p>
-            <Link href="/bespoke">Bespoke</Link>
-            <p className="text-base md:text-lg lg:text-xl">About</p>
+            {filteredPages.map((page) => (
+              <Link href={page.path}>{page.name}</Link>
+            ))}
           </div>
         </div>
       </nav>
@@ -39,12 +39,12 @@ const Navbar = () => {
           
           {/* Left section */}
           <div className="flex-1">
-            <p className="text-base md:text-lg lg:text-xl">{currentPage}</p>
+            <Link href="/">Wyfe</Link>
           </div>
 
           {/* Centre section */}
-          <div className="flex-none text-center">
-            <Link href="/">Wyfe</Link>
+          <div className="flex--none text-center">
+            <Link href="/">{currentPage}</Link>
           </div>
 
           {/* Right section */}
