311bc71d515346c9fc6a6259257500f6e13e845d
Simon Budden <simon.budden@gmail.com>
2024-08-06 16:17:40 +0100

    Page header component created, bespoke page redone

--- app/bespoke/page.js
+++ app/bespoke/page.js
@@ -26,10 +26,16 @@ import BottomRightImage from '../../public/mood/bespoke/IMG_5975.jpg'
 //     BottomRightImage = '/unsplash/bespoke/IMG_5975.jpg';
 // }
 
+// Email button backend
+const email = "hello@wyfe.co.uk"
+const subject = "Tell me about bespoke dresses"
+const body = `Hi Wyfe,\n\nI'd like to know more about your bespoke dresses.\n\nWedding date: [enter your wedding date]\nBudget: [enter your budget]\nWhat I'm looking for: [enter your ideas]\n\nThanks,`
+const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
 const Bespoke = () => {
     return (
         <div className="flex flex-col min-h-screen relative">
-            <Navbar />
+            {/* <Navbar /> */}
 
             <PageHeader 
                 image={ TopRightImage }
@@ -45,8 +51,8 @@ const Bespoke = () => {
                         </p>
                     </div>
                     
-                    {/* Top images */}
-                    <div className="flex justify-center gap-4 p-4">
+                    {/* Top section */}
+                    <div className="flex justify-center items-center gap-4 p-4">
                         <Image 
                             src={TopLeftImage}
                             alt="Bespoke 1"
@@ -54,37 +60,43 @@ const Bespoke = () => {
                         />
                         <div className="px-4 py-8 text-center font-georgia-italic max-w-3xl mx-auto">
                             <p className="text-sm md:text-base lg:text-base">
-                                [Bespoke introduction] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis diam sit amet risus efficitur porta. Cras venenatis accumsan ornare. Aenean mattis ligula mi, vel gravida tellus porta eget. Proin posuere feugiat ante sed convallis.
+                            [Description of exerience] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis diam sit amet risus efficitur porta. Cras venenatis accumsan ornare. Aenean mattis ligula mi, vel gravida tellus porta eget. Proin posuere feugiat ante sed convallis. Donec vehicula sem eget mauris lobortis scelerisque. Aliquam finibus non quam non laoreet.
                             </p>
                         </div>
                     </div>
                     
-                    {/* Text */}
-                    <div className="px-4 py-8 text-center font-georgia-italic max-w-3xl mx-auto">
+                    {/* Call to action text */}
+                    <div className="px-4 pb-2 pt-4 text-center font-georgia-italic max-w-3xl mx-auto">
                         <p className="text-sm md:text-base lg:text-base">
                         [Call to action] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis diam sit amet risus efficitur porta. Cras venenatis accumsan ornare. Aenean mattis ligula mi, vel gravida tellus porta eget. Proin posuere feugiat ante sed convallis. Donec vehicula sem eget mauris lobortis scelerisque. Aliquam finibus non quam non laoreet.
                         </p>
                     </div>
 
-                    {/* Middle images */}
-                    <div className="flex justify-center gap-4 p-4">
-                        <Image 
-                            src={BottomLeftImage}
-                            alt="Bespoke 3"
-                            className="w-1/2 h-auto object-cover"
-                        />
-                        <Image 
-                            src={BottomRightImage}
-                            alt="Bespoke 4"
-                            className="w-1/2 h-auto object-cover"
-                        />
+                    {/* Call to action button */}
+                    <div className="flex justify-center items-center pt-2 pb-4">
+                        <a href={mailtoLink}>
+                            <button className="font-georgia-italic bg-black border-2 border-white hover:bg-white text-white hover:text-black py-2 px-4">
+                                Inquire about bespoke a dress
+                            </button>
+                        </a>
                     </div>
-                    
-                    {/* Text */}
-                    <div className="px-4 py-8 text-center font-georgia-italic max-w-3xl mx-auto">
-                        <p className="text-sm md:text-base lg:text-base">
-                        [Description of exerience] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mattis diam sit amet risus efficitur porta. Cras venenatis accumsan ornare. Aenean mattis ligula mi, vel gravida tellus porta eget. Proin posuere feugiat ante sed convallis. Donec vehicula sem eget mauris lobortis scelerisque. Aliquam finibus non quam non laoreet.
-                        </p>
+
+                    {/* bottom images */}
+                    <div className="flex justify-center gap-4 p-4">
+                        <div className="w-1/2 h-[50vh] flex items-center">
+                            <Image 
+                                src={BottomLeftImage}
+                                alt="Bespoke 3"
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
+                        <div className="w-1/2 h-[50vh] flex items-center">
+                            <Image 
+                                src={BottomRightImage}
+                                alt="Bespoke 4"
+                                className="w-full h-full object-cover"
+                            />
+                        </div>
                     </div>
                 </div>
             </main>
--- components/PageHeader/Index.jsx
+++ components/PageHeader/Index.jsx
@@ -4,6 +4,8 @@ import React from 'react';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
+import Navbar from '../Navbar';
+
 const pathnameToTitle = {
     '/': '',
     '/about': 'About',
@@ -17,24 +19,29 @@ const PageHeader = ({ image }) => {
     const title = pathnameToTitle[currentPath] || '';
   
     return (
-    <div className="relative w-full h-64 md:h-80 lg:h-96">
-        {/* Header image */}
-        <Image
-            src={image}
-            alt={title}
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-            className="w-full h-full"
-        />
-        {/* Overlay Text */}
-        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-georgia-italic font-normal">
-            {title}
-            </h1>
+        <div className="relative w-full h-64 md:h-80 lg:h-96">
+            {/* Header image */}
+            <Image
+                src={image}
+                alt={title}
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                className="w-full h-full"
+            />
+            {/* Navbar */}
+            <div className="absolute top-0 left-0 mt-10 w-full z-20">
+                <Navbar />
+            </div>
+
+            {/* Overlay Text */}
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                <h1 className="text-white text-2xl md:text-4xl lg:text-5xl font-georgia-italic font-normal">
+                    {title}
+                </h1>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default PageHeader;
